831e24f409510e7a747d561fc6a7612ddb9c26e1
kolya-t <toporkov.kolya96@gmail.com>
2017-10-29 20:20:58 +0300

    New clientId

--- src/actions/authActionCreators.js
+++ src/actions/authActionCreators.js
@@ -5,7 +5,7 @@ export const loadGapi = () => dispatch => {
   gapi.load('client:auth2', () => {
     gapi.client.init({
       discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"],
-      clientId: '716950246214-d1cddjfk2f2lhh7h2sshr1pbm10v2jkd.apps.googleusercontent.com',
+      clientId: '716950246214-rhreooejlqbf3v87u56cglgf1nneqs6u.apps.googleusercontent.com',
       scope: 'https://www.googleapis.com/auth/gmail.readonly'
     }).then(() => {
       dispatch(gapiLoadSuccess());
